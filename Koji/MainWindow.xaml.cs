5e4e1c7a4b6bf7307bd796744adda5fe6c37b484
Aviram Hassan <aviramketchup@gmail.com>
2015-04-05 13:45:24 +0300

    * Initial commit

---
+++ Koji/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+
+
+namespace Koji
+{
+    /// <summary>
+    /// Constants that will be used across the namespace.
+    /// </summary>
+    internal static class Constants
+    {
+        /// <summary>
+        /// Sound related constants.
+        /// </summary>
+        public const string SOUND_EXT_FILTER = "Sound files |*.wav;*.mp3";
+
+        public static readonly string[] VALID_EXT_TYPE =
+        {
+            ".mp3",
+            ".wav",
+        };
+
+}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Enables user adding of new sound to Koji.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog   = new OpenFileDialog();
+
+            openFileDialog.Filter           = Constants.SOUND_EXT_FILTER;
+            openFileDialog.RestoreDirectory = true;
+
+            Nullable<bool> dialogSuccess = openFileDialog.ShowDialog();
+
+            if (null == dialogSuccess)
+            {
+                throw new ArgumentNullException("Dialog returned null");
+            }
+
+            if (false == dialogSuccess)
+            {
+                return;
+            }
+
+        }
+    }
+}
---
+++ Koji/Sound.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koji
+{
+    public class Sound
+    {
+        protected enum SoundStates
+        {
+            PLAYING,
+            PAUSED,
+            STOPPED
+        };
+        
+        /// Private Members
+        
+        ///<summary>
+        /// Path to the sound file.
+        /// </summary>
+        protected string FilePath;
+
+        ///<summary>
+        /// The sound file extension.
+        /// </summary>
+        protected string FileType;
+
+        ///<summary>
+        /// Volume to play the sound at.
+        /// </summary>
+        protected int Volume;
+
+        ///<summary>
+        /// Current state of the Sound.
+        /// </summary>
+        protected SoundStates CurrentState;
+
+        /// <summary>
+        /// Sets the instance variable of FilePath to the given path if it has a valid extension.
+        /// </summary>
+        /// <param name="PathToFile"></param>
+        /// <returns></returns>
+        public bool SetFile(string PathToFile)
+        {
+            if (File.Exists(PathToFile) &&
+                Constants.VALID_EXT_TYPE.Contains(Path.GetExtension(PathToFile))
+                )
+            {
+                FilePath = PathToFile;
+                FileType = Path.GetExtension(PathToFile);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the sound file path.
+        /// </summary>
+        /// <returns>String path to file.</returns>
+        public string GetFile()
+        {
+            return FilePath;
+        }
+
+    }
+}
